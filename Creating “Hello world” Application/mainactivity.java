b23369c331cafd0567198aa8f60e2ec1c9490e66
Mohammad Abdul Khadar <abdul.khadar.tech@gmail.com>
2026-05-18 19:00:35 +0530

    Update mainactivity.java

--- Creating “Hello world” Application/mainactivity.java
+++ Creating “Hello world” Application/mainactivity.java
@@ -1,24 +1,25 @@
-package com.example.myapplication1; 
-import android.os.Bundle; 
-import android.view.View; 
-import android.widget.Button; 
-import android.widget.Toast; 
-import androidx.appcompat.app.AppCompatActivity; 
-import androidx.activity.EdgeToEdge; 
-import androidx.core.graphics.Insets; 
-import androidx.core.view.ViewCompat; 
-import androidx.core.view.WindowInsetsCompat; 
-public class MainActivity extends AppCompatActivity { 
-@Override 
-protected void onCreate(Bundle savedInstanceState) { 
-super.onCreate(savedInstanceState); 
-setContentView(R.layout.activity_main); 
-Button b = findViewById(R.id.hello); 
-b.setOnClickListener(new View.OnClickListener() { 
-@Override 
-public void onClick(View v) { 
-Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show(); 
-} 
-}); 
-} 
-}           
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnClick;
+    TextView txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnClick = findViewById(R.id.btnClick);
+        txtMessage = findViewById(R.id.txtMessage);
+
+        btnClick.setOnClickListener(view -> {
+            txtMessage.setText("Hey! We are using Android Application");
+        });
+    }
+}
