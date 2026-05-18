df6924cd3d01ceefe7c6edfb57905c56523936e2
Mohammad Abdul Khadar <abdul.khadar.tech@gmail.com>
2026-05-18 19:10:02 +0530

    Update mainactivity.java

--- Creating message screen orientation/mainactivity.java
+++ Creating message screen orientation/mainactivity.java
@@ -1,38 +1,31 @@
-package com.example.myapplication3; 
-import android.content.pm.ActivityInfo; 
-import android.os.Bundle; 
-import android.view.View; 
-import android.widget.Toast; 
-import android.widget.Button; 
-import androidx.activity.EdgeToEdge; 
-import androidx.appcompat.app.AppCompatActivity; 
-import androidx.core.graphics.Insets; 
-import androidx.core.view.ViewCompat; 
-import androidx.core.view.WindowInsetsCompat; 
+package com.example.rehan;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    @Override 
-    protected void onCreate(Bundle savedInstanceState) { 
-        super.onCreate(savedInstanceState); 
-        EdgeToEdge.enable(this); 
-        setContentView(R.layout.activity_main); 
-        Button l,p; 
-        l = findViewById(R.id.button); 
-        p = findViewById(R.id.button2); 
-        l.setOnClickListener(new View.OnClickListener() { 
-            @Override 
-            public void onClick(View v) {         
-setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); 
-            Toast.makeText(MainActivity.this, "Hey We are in Landscape", 
-Toast.LENGTH_SHORT).show(); 
-            }
+
+    Button btnPortrait, btnLandscape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPortrait = findViewById(R.id.btnPortrait);
+        btnLandscape = findViewById(R.id.btnLandscape);
+
+        btnPortrait.setOnClickListener(view -> {
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         });
-        p.setOnClickListener(new View.OnClickListener() { 
-            @Override 
-            public void onClick(View v) { 
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); 
-                Toast.makeText(MainActivity.this, "Hey We are in Portrait Orientation", 
-                        Toast.LENGTH_SHORT).show();
-            }
+
+        btnLandscape.setOnClickListener(view -> {
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         });
     }
 }
